01a84f9515195f2f0eff6a8ec906cc946bd8f81b
Brett O'Donnell <cornernote@gmail.com>
2013-09-22 06:28:25 +0930

    tuning

--- app/views/layouts/_content.php
+++ app/views/layouts/_content.php
@@ -19,34 +19,39 @@ if ($this->showNavBar) {
 }
 ?>
 <div class="holder">
-    <div id="body" class="container-fluid">
-        <?php
-        if ($this->pageHeading) {
-            echo '<h1 class="header">' . $this->pageHeading . '</h1>';
-        }
-        if ($this->menu) {
-            $this->widget('bootstrap.widgets.TbMenu', array(
-                'id' => 'menu',
-                'type' => 'tabs',
-                'items' => $this->menu,
-            ));
-        }
-        if ($this->breadcrumbs) {
-            $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
-                'links' => $this->breadcrumbs,
-                'separator' => '',
-            ));
-        }
-        echo user()->multiFlash();
-        echo $content;
-        ?>
-    </div>
-    <footer class="footer">
-        <div class="container-fluid">
+    <div id="body">
+        <header class="container-fluid">
             <?php
-            $this->renderPartial('dressing.views.audit._footer');
-            echo '<p class="pull-right">' . l(t('Back to Top') . ' &uarr;', '#') . '</p>';
+            if ($this->pageHeading) {
+                echo '<h1 class="header">' . $this->pageHeading . '</h1>';
+            }
+            if ($this->menu) {
+                $this->widget('bootstrap.widgets.TbMenu', array(
+                    'id' => 'menu',
+                    'type' => 'tabs',
+                    'items' => $this->menu,
+                ));
+            }
+            if ($this->breadcrumbs) {
+                $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
+                    'id' => 'breadcrumb',
+                    'links' => $this->breadcrumbs,
+                    'separator' => '',
+                ));
+            }
+            ?>
+        </header>
+        <div id="content" class="container-fluid">
+            <?php
+            echo user()->multiFlash();
+            echo $content;
             ?>
         </div>
+    </div>
+    <footer class="container-fluid">
+        <?php
+        $this->renderPartial('dressing.views.audit._footer');
+        echo '<p class="pull-right">' . l(t('Back to Top') . ' &uarr;', '#') . '</p>';
+        ?>
     </footer>
 </div>
