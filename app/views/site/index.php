627829af71d6f8636d48d4394457b29ff6d09cf6
Brett O'Donnell <cornernote@gmail.com>
2013-09-22 05:10:07 +0930

    tuning

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -7,7 +7,11 @@ $this->pageTitle = $this->pageHeading = Yii::app()->name;
 
 echo '<p>You may change the content of this page by modifying the file <code>' . __FILE__ . '</code>.</p>';
 if (!YdHelper::tableExists('migration')) {
-    echo '<p>To install the database run <code>' . Yii::getPathOfAlias('core') . DS . 'bin' . DS . 'yiic migrate</code>.</p>';
+    echo '<p>To install the database run:</p>';
+    echo '<pre>';
+    echo 'cp ' . Yii::getPathOfAlias('dressing') . DS . 'migrations' . DS . '*.[php|sql] ' . Yii::getPathOfAlias('application') . DS . 'migrations' . "\n";
+    echo Yii::getPathOfAlias('core') . DS . 'bin' . DS . 'yiic migrate' . "\n";
+    echo '</pre>';
 }
 
 
