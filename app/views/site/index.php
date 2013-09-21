ea90b6124b6acbfa81e759ee6d73547cf5ee6961
Brett O'Donnell <cornernote@gmail.com>
2013-09-22 05:40:53 +0930

    tuning

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -9,7 +9,8 @@ echo '<p>You may change the content of this page by modifying the file <code>' .
 if (!YdHelper::tableExists('migration')) {
     echo '<p>To install the database run:</p>';
     echo '<pre>';
-    echo 'cp ' . Yii::getPathOfAlias('dressing') . DS . 'migrations' . DS . '*.[php|sql] ' . Yii::getPathOfAlias('application') . DS . 'migrations' . "\n";
+    $win = YdHelper::isWindowsServer();
+    echo ($win ? 'copy' : 'cp') . ' ' . Yii::getPathOfAlias('dressing') . DS . 'migrations' . DS . '*' . ($win ? '.*' : '') . ' ' . Yii::getPathOfAlias('application') . DS . 'migrations' . "\n";
     echo Yii::getPathOfAlias('core') . DS . 'bin' . DS . 'yiic migrate' . "\n";
     echo '</pre>';
 }
