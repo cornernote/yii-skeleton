f408be76708da74a8dea6b81952761f1c682cf62
Brett O'Donnell <cornernote@gmail.com>
2013-09-21 11:33:07 +0930

    tuning

--- public/index.php
+++ public/index.php
@@ -3,20 +3,20 @@
 // start the timer
 $_ENV['_start'] = microtime(true);
 
-// load core settings
-$core = dirname(__FILE__) . '/../app/core.php';
-if (!file_exists($core)) {
-	trigger_error('cannot find core file at "' . $core . '"', E_USER_ERROR);
+// load settings
+$config = dirname(__FILE__) . '/../config.php';
+if (!file_exists($config)) {
+	trigger_error('cannot find config file at "' . $config . '"', E_USER_ERROR);
 }
-$_ENV['_core'] = require($core);
+$_ENV['_config'] = require($config);
 
 // set debug levels
-if ($_ENV['_core']['setting']['debug']) {
+if ($_ENV['_config']['setting']['debug']) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
     ini_set('log_errors', 1);
     defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', $_ENV['_core']['setting']['debug']);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', $_ENV['_config']['setting']['debug']);
 }
 else {
     error_reporting(E_ALL);
@@ -26,24 +26,24 @@ else {
 }
 
 // include global functions
-$globals = dirname(__FILE__) . '/../app/' . $_ENV['_core']['setting']['app_version'] . '/globals.php';
+$globals = dirname(__FILE__) . '/../' . $_ENV['_config']['setting']['app_version'] . '/globals.php';
 if (!file_exists($globals)) {
 	trigger_error('cannot find globals file at "' . $globals . '"', E_USER_ERROR);
 }
 require_once($globals);
 
 // define path to congig
-$config = dirname(__FILE__) . '/../app/' . $_ENV['_core']['setting']['app_version'] . '/config/web.php';
+$config = dirname(__FILE__) . '/../' . $_ENV['_config']['setting']['app_version'] . '/config/web.php';
 if (!file_exists($config)) {
 	trigger_error('cannot find config file at "' . $config . '"', E_USER_ERROR);
 }
 
 // include Yii or YiiLite
-if ($_ENV['_core']['setting']['yii_lite']) {
-    $yii = dirname(__FILE__) . '/../vendors/yii/' . $_ENV['_core']['setting']['yii_version'] . '/framework/yiilite.php';
+if ($_ENV['_config']['setting']['yii_lite']) {
+    $yii = dirname(__FILE__) . '/../vendors/yii/' . $_ENV['_config']['setting']['yii_version'] . '/framework/yiilite.php';
 }
 else {
-    $yii = dirname(__FILE__) . '/../vendors/yii/' . $_ENV['_core']['setting']['yii_version'] . '/framework/yii.php';
+    $yii = dirname(__FILE__) . '/../vendors/yii/' . $_ENV['_config']['setting']['yii_version'] . '/framework/yii.php';
 }
 if (!file_exists($yii)) {
 	trigger_error('cannot find framework file at "' . $yii . '"', E_USER_ERROR);
