e209ef8ef288650831b5ffec2a3b3091a4c1c3d2
Brett O'Donnell <cornernote@gmail.com>
2013-10-03 02:44:29 +0930

    echo instead of html in ajax layout

--- app/views/layouts/ajax.php
+++ app/views/layouts/ajax.php
@@ -3,12 +3,9 @@
  * @var $this YdWebController
  * @var $content
  */
-?>
 
-<div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal"><i class="icon-remove"></i></button>
-    <h3><?php echo $this->pageHeading; ?></h3>
-</div>
-<?php
+echo '<div class="modal-header">';
+echo '<button type="button" class="close" data-dismiss="modal"><i class="icon-remove"></i></button>';
+echo '<h3>' . $this->pageHeading . '</h3>';
 echo $content;
-?>
+echo '</div>';
