c17eabafd26f0084adbb3ac0852a9f63661e3cfa
Brett O'Donnell <cornernote@gmail.com>
2013-10-06 20:50:20 +1030

    tweak copyright

--- app/themes/lite/views/layouts/_theme_scripts.php
+++ app/themes/lite/views/layouts/_theme_scripts.php
@@ -2,7 +2,7 @@
 /**
  * @author Brett O'Donnell <cornernote@gmail.com>
  * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Brett O'Donnell <cornernote@gmail.com>, Zain Ul abidin <zainengineer@gmail.com>
+ * @copyright 2013 Mr PHP
  * @link https://github.com/cornernote/yii-skeleton
  * @license http://www.gnu.org/copyleft/gpl.html
  */
