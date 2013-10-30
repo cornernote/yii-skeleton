839d0b71399147e0e800086a0255d37bb7f3d92c
Brett O'Donnell <cornernote@gmail.com>
2013-10-30 16:43:00 +1030

    Create Yii.php

---
+++ app/components/yii/Yii.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * NOTE: This file is not loaded.
+ * It exists so storm links application components correctly.
+ *
+ * @method WebApplication static app() app()
+ *
+ * @author Brett O'Donnell <cornernote@gmail.com>
+ * @author Zain Ul abidin <zainengineer@gmail.com>
+ * @copyright 2013 Mr PHP
+ * @link https://github.com/cornernote/yii-skeleton
+ * @license http://www.gnu.org/copyleft/gpl.html
+ */
+class Yii extends YiiBase
+{
+
+    /**
+     * @return WebApplication
+     */
+    public static function app()
+    {
+        return parent::app();
+    }
+
+}
