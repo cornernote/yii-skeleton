bb1e31c8b288e5685548bc08c158c6ce963dce3c
Brett O'Donnell <cornernote@gmail.com>
2013-10-30 16:49:44 +1030

    Update yiic.php

--- yiic.php
+++ yiic.php
@@ -7,7 +7,7 @@
 $_ENV['_start'] = microtime(true);
 
 // include globals
-require_once(dirname(__FILE__) . DS . 'app' . DS . 'globals.php');
+require_once(dirname(__FILE__) . '/app/globals.php');
 
 // run yii-dressing
-require_once(dirname(__FILE__) . DS . 'vendor' . DS . 'mrphp' . DS . 'yii-dressing' . DS . 'src' . DS . 'entry' . DS . 'yiic.php');
+require_once(dirname(__FILE__) . '/vendor/mrphp/yii-dressing/src/entry/yiic.php');
