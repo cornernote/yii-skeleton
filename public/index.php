f6e68d1a001f7c7eab02b1e08585257319009d9b
Brett O'Donnell <cornernote@gmail.com>
2013-10-30 16:48:01 +1030

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,9 +6,6 @@
 // start the timer
 $_ENV['_start'] = microtime(true);
 
-// define directory separator shortcut
-defined('DS') or define('DS', DIRECTORY_SEPARATOR);
-
 // include globals
 require_once(dirname(dirname(__FILE__)) . DS . 'app' . DS . 'globals.php');
 
