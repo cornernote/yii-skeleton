52e06bac1ce1eef882d3359510caf291a1bff203
Brett O'Donnell <cornernote@gmail.com>
2013-11-01 15:25:58 +1030

    Create construction.php

---
+++ public/construction.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Under Construction</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="container">
+
+    <h1>Under Construction</h1>
+
+    <p>Please check back soon.</p>
+
+
+</div>
+
+</body>
+</html>
