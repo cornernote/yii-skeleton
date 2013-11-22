a8f72ee1de1f6950f0a8f43301d7319ffa43f681
Brett O'Donnell <cornernote@gmail.com>
2013-11-22 22:59:48 +1030

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-defined('YII_DRESSING_PATH') or define('YII_DRESSING_PATH', dirname(__FILE__) . '/vendors/mrphp/yii-dressing/src');
-require_once(YII_DRESSING_PATH . '/components/YdConfig.php');
+defined('YII_DRESSING_PATH') or define('YII_DRESSING_PATH', dirname(__FILE__) . '/vendor/mrphp/yii-dressing/src');
+require_once(YII_DRESSING_PATH . '/YdConfig.php');
 
 /**
  * Config is a helper class serving pre-application configuration.
@@ -10,5 +10,14 @@ require_once(YII_DRESSING_PATH . '/components/YdConfig.php');
  */
 class Config extends YdConfig
 {
+  
+    /**
+     * Force loading config.json
+     * @param null $file
+     */
+    public function __construct($file = null)
+    {
+        parent::__construct(dirname(__FILE__) . 'config.json');
+    }
 
 }
