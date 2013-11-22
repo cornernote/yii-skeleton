ac384766eef50a13d971ad1d865800c723c3625e
Brett O'Donnell <cornernote@gmail.com>
2013-11-22 22:34:45 +1030

    Update yii.php

--- app/yii.php
+++ app/yii.php
@@ -2,13 +2,16 @@
 require_once(YII_DRESSING_PATH . '/YdBase.php');
 
 /**
- * Class Yii
+ * Yii is a helper class serving common framework functionalities.
+ *
+ * It encapsulates {@link YdBase} which provides the actual implementation.
+ * By writing your own Yii class, you can customize some functionalities of YdBase.
  */
 class Yii extends YdBase
 {
 
     /**
-     * @return Application
+     * @return Application this method exists so phpStorm will code complete correctly for Yii::app().
      */
     public static function app()
     {
