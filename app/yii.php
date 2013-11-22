632ef9b3b118a43475bbfa35dc4939af15dec67a
Brett O'Donnell <cornernote@gmail.com>
2013-11-22 21:39:56 +1030

    Create yii.php

---
+++ app/yii.php
@@ -0,0 +1,18 @@
+<?php
+require_once(YII_DRESSING_PATH . DS . 'YdBase.php');
+
+/**
+ * Class Yii
+ */
+class Yii extends YdBase
+{
+
+    /**
+     * @return Application
+     */
+    public static function app()
+    {
+        return parent::app();
+    }
+
+}
