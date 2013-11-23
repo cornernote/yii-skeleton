c82c959a6ba9a00beb27a9b5db1c5120155448b1
Brett O'Donnell <cornernote@gmail.com>
2013-11-23 21:39:52 +1030

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-defined('YII_DRESSING_PATH') or define('YII_DRESSING_PATH', dirname(__FILE__) . '/vendor/mrphp/yii-dressing/src');
+defined('YII_DRESSING_PATH') or define('YII_DRESSING_PATH', dirname(__FILE__) . '/vendor/mrphp/yii-dressing/yii-dressing');
 require_once(YII_DRESSING_PATH . '/YdConfig.php');
 
 /**
