5e6732f92e3b170f2f81c5a1c3fa5c55524aef88
Brett O'Donnell <cornernote@gmail.com>
2013-11-30 14:07:15 +1030

    Update index.php

--- public/index.php
+++ public/index.php
@@ -12,11 +12,11 @@ defined('YII_BEGIN_TIME') or define('YII_BEGIN_TIME', microtime(true));
  * Setup the environment
  */
 require_once(dirname(dirname(__FILE__)) . '/config.php');
-Config::createInstance();
+$config = Config::createInstance();
 
 /**
  * run the Yii app (Yii-Haw!)
  */
 require_once(APP_PATH . DS . 'globals.php');
 require_once(APP_PATH . DS . 'yii.php');
-Yii::createWebApplication(APP_CONFIG)->run();
+Yii::createWebApplication($config->getAppConfig())->run();
