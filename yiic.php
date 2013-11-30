c676a86ce96aa604036951ba97c34a924bee26a9
Brett O'Donnell <cornernote@gmail.com>
2013-11-30 14:07:08 +1030

    Update yiic.php

--- yiic.php
+++ yiic.php
@@ -12,11 +12,11 @@ defined('YII_BEGIN_TIME') or define('YII_BEGIN_TIME', microtime(true));
  * Setup the environment
  */
 require_once(dirname(__FILE__) . '/config.php');
-Config::createInstance();
+$config = Config::createInstance();
 
 /**
  * run the Yii app (Yii-Haw!)
  */
 require_once(APP_PATH . DS . 'globals.php');
 require_once(APP_PATH . DS . 'yii.php');
-Yii::createConsoleApplication(APP_CONFIG)->run();
+Yii::createConsoleApplication($config->getAppConfig())->run();
