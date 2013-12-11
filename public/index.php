7434aa6ec045e93bb42b4d69fc9efab1657d68ea
cornernote <cornernote@gmail.com>
2013-12-12 00:21:34 +1030

    update license

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 /**
  * Application Web Entry Script
+ *
+ * @author Brett O'Donnell <cornernote@gmail.com>
+ * @author Zain Ul abidin <zainengineer@gmail.com>
+ * @copyright 2013 Mr PHP
+ * @link https://github.com/cornernote/yii-skeleton
+ * @license https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 
 /**
--- yiic.php
+++ yiic.php
@@ -1,6 +1,12 @@
 <?php
 /**
  * Application CLI Entry Script
+ *
+ * @author Brett O'Donnell <cornernote@gmail.com>
+ * @author Zain Ul abidin <zainengineer@gmail.com>
+ * @copyright 2013 Mr PHP
+ * @link https://github.com/cornernote/yii-skeleton
+ * @license https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 
 /**
