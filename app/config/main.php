b52830481161fda1d2e9081ea8af07d7fbde1ec6
Brett O'Donnell <cornernote@gmail.com>
2013-12-11 23:51:00 +1030

    Update main.php

--- app/config/main.php
+++ app/config/main.php
@@ -67,6 +67,113 @@ return array(
             'baseUrl' => PUBLIC_URL . '/assets',
             'linkAssets' => true,
         ),
+        'dressing' => array(
+            'class' => 'dressing.YdDressing',
+        ),
+        'errorHandler' => array(
+            'class' => 'dressing.components.YdErrorHandler',
+            'errorAction' => 'site/error',
+        ),
+        'fatalErrorCatch' => array(
+            'class' => 'dressing.components.YdFatalErrorCatch',
+        ),
+        'user' => array(
+            'class' => 'dressing.components.YdWebUser',
+            'allowAutoLogin' => true,
+            'loginUrl' => array('/account/login'),
+            'behaviors' => array(
+                'webUserFlash' => array(
+                    'class' => 'dressing.behaviors.YdWebUserFlashBehavior',
+                ),
+            ),
+        ),
+        'returnUrl' => array(
+            'class' => 'dressing.components.YdReturnUrl',
+        ),
+        'bootstrap' => array(
+            'class' => 'bootstrap.components.Bootstrap',
+            'fontAwesomeCss' => true,
+        ),
+        'urlManager' => array(
+            'urlFormat' => isset($_GET['r']) ? 'get' : 'path', // allow filters in audit/index work
+            'showScriptName' => false,
+        ),
+        'cacheFile' => array(
+            'class' => 'CFileCache',
+        ),
+        'cacheDb' => array(
+            'class' => 'CDbCache',
+        ),
+        'cacheApc' => array(
+            'class' => 'CApcCache',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+        ),
+        'clientScript' => array(
+            'class' => 'YdClientScript',
+        ),
+        'session' => array(
+            'class' => 'CCacheHttpSession',
+            'cacheID' => 'cacheApc',
+        ),
+        'email' => array(
+            'class' => 'dressing.components.YdEmail',
+        ),
+        'swiftMailer' => array(
+            'class' => 'dressing.components.YdSwiftMailer',
+        ),
+        'auditTracker' => array(
+            'class' => 'dressing.components.YdAuditTracker',
+        ),
+        'format' => array(
+            'class' => 'dressing.components.YdFormatter',
+        ),
+        'reCapture' => array(
+            'class' => 'dressing.components.YdReCapture',
+        ),
+        'widgetFactory' => array(
+            'widgets' => array(
+                'TbMenu' => array(
+                    'activateParents' => true,
+                ),
+                'TbCKEditor' => array(
+                    'editorOptions' => array(
+                        // 'toolbar_Full' => array(
+                        //     array('name' => 'document', 'items' => array('Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates')),
+                        //     array('name' => 'clipboard', 'items' => array('Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo')),
+                        //     array('name' => 'editing', 'items' => array('Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt')),
+                        //     array('name' => 'forms', 'items' => array('Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField')),
+                        //     array('name' => 'basicstyles', 'items' => array('Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat')),
+                        //     array('name' => 'paragraph', 'items' => array('NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl')),
+                        //     array('name' => 'links', 'items' => array('Link', 'Unlink', 'Anchor')),
+                        //     array('name' => 'insert', 'items' => array('Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe')),
+                        //     array('name' => 'styles', 'items' => array('Styles', 'Format', 'Font', 'FontSize')),
+                        //     array('name' => 'colors', 'items' => array('TextColor', 'BGColor')),
+                        //     array('name' => 'tools', 'items' => array('Maximize', 'ShowBlocks', '-', 'About')),
+                        // ),
+                        // 'toolbar_Basic' => array(
+                        //     array('Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink', '-', 'About'),
+                        // ),
+                        'toolbar_Full' => array(
+                            array('name' => 'tools', 'items' => array('Source', 'Maximize', 'ShowBlocks')),
+                            array('name' => 'clipboard', 'items' => array('Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo')),
+                            array('name' => 'basicstyles', 'items' => array('Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat')),
+                            array('name' => 'paragraph', 'items' => array('NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock')),
+                            array('name' => 'links', 'items' => array('Link', 'Unlink', 'Anchor')),
+                            array('name' => 'insert', 'items' => array('Image', 'Table', 'HorizontalRule', 'SpecialChar')),
+                            array('name' => 'styles', 'items' => array('Format')),
+                        ),
+                        'toolbar_Basic' => array(
+                            array('Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink'),
+                        ),
+                        'toolbar' => 'Full',
+                    ),
+                ),
+            ),
+        ),
+
+        
     ),
     'preload' => array(
         'log',
@@ -75,5 +182,30 @@ return array(
         'dressing',
         'bootstrap',
     ),
-
+    'controllerMap' => array(
+        'account' => 'dressing.controllers.YdAccountController',
+        'attachment' => 'dressing.controllers.YdAttachmentController',
+        'audit' => 'dressing.controllers.YdAuditController',
+        'auditTrail' => 'dressing.controllers.YdAuditTrailController',
+        'contactUs' => 'dressing.controllers.YdContactUsController',
+        'emailSpool' => 'dressing.controllers.YdEmailSpoolController',
+        'emailTemplate' => 'dressing.controllers.YdEmailTemplateController',
+        'error' => 'dressing.controllers.YdErrorController',
+        'lookup' => 'dressing.controllers.YdLookupController',
+        'siteMenu' => 'dressing.controllers.YdSiteMenuController',
+        'role' => 'dressing.controllers.YdRoleController',
+        'setting' => 'dressing.controllers.YdSettingController',
+        'user' => 'dressing.controllers.YdUserController',
+    ),
+    'commandMap' => array(
+        'migrate' => array(
+            'class' => 'system.cli.commands.MigrateCommand',
+            'migrationPath' => 'application.migrations',
+            'migrationTable' => 'migration',
+            'connectionID' => 'db',
+            'templateFile' => 'dressing.migrations.templates.migrate_template',
+        ),
+        'emailSpool' => 'dressing.commands.YdEmailSpoolCommand',
+        'errorEmail' => 'dressing.commands.YdErrorEmailCommand',
+    ),
 );
