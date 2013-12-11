2da55042fae46bc066ffe692cdc7a77db2f2cfef
Brett O'Donnell <cornernote@gmail.com>
2013-12-12 00:47:10 +1030

    Update GlobalInit.php

--- app/components/GlobalInit.php
+++ app/components/GlobalInit.php
@@ -5,15 +5,32 @@
 class GlobalInit
 {
 
+    /**
+     * Initilize the application
+     */
     public function init()
     {
+        parent::init();
+        $app = Yii::app();
+        $user = $app->user;
 
-        // fake controller
-        $controller = new CController('preloadController');
-        
-        // load QTip on every page
-        $controller->widget('dressing.widgets.YdQTip');
-        
+        if (!$app->request->isAjaxRequest) {
+            // fake controller
+            $controller = new CController('preloadController');
+    
+            // set user theme
+            if ($user->user && $user->user->asa('EavBehavior'))
+                if ($theme = $user->user->getEavAttribute('theme'))
+                    $app->setTheme($theme);
+    
+            // init widgets
+            if (!$app->request->isAjaxRequest) {
+                $app->clientScript->registerCSSFile($app->dressing->getAssetsUrl() . '/css/yii-dressing.css');
+                $this->widget('dressing.widgets.YdModal');
+                $this->widget('dressing.components.YdFancyBox');
+                $this->widget('dressing.widgets.YdQTip');
+            }
+        }
     }
 
 }
