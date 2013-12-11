86a4d7b858cac42202f772fcf47ba7ce350c347b
Brett O'Donnell <cornernote@gmail.com>
2013-12-12 00:45:36 +1030

    Update WebController.php

--- app/components/WebController.php
+++ app/components/WebController.php
@@ -32,30 +32,6 @@ class WebController extends Controller
      */
     public $layout = 'dressing.views.layouts.default';
 
-
-    /**
-     * Initializes the controller.
-     */
-    public function init()
-    {
-        parent::init();
-        $app = Yii::app();
-        $user = $app->user;
-
-        // set user theme
-        if ($user->user && $user->user->asa('EavBehavior'))
-            if ($theme = $user->user->getEavAttribute('theme'))
-                $app->setTheme($theme);
-
-        // init widgets
-        if (!$app->request->isAjaxRequest) {
-            $app->clientScript->registerCSSFile($app->dressing->getAssetsUrl() . '/css/yii-dressing.css');
-            $this->widget('dressing.widgets.YdModal');
-            $this->widget('dressing.components.YdFancyBox');
-            $this->widget('dressing.widgets.YdQTip');
-        }
-    }
-
     /**
      * @return array
      */
