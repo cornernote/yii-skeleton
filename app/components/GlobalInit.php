ac45b49ebbd641f019629753887f811c1908cb48
Brett O'Donnell <cornernote@gmail.com>
2013-12-12 00:44:04 +1030

    Update GlobalInit.php

--- app/components/GlobalInit.php
+++ app/components/GlobalInit.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * GlobalInit
+ * GlobalInit is loaded (preloaded) directly after CWebApplication has been loaded
  */
 class GlobalInit
 {
@@ -8,7 +8,12 @@ class GlobalInit
     public function init()
     {
 
-
+        // fake controller
+        $controller = new CController('preloadController');
+        
+        // load QTip on every page
+        $controller->widget('dressing.widgets.YdQTip');
+        
     }
 
 }
