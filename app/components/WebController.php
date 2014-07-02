16e35e91df35e04aae7ac704ec64f2eafec4db02
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:29:12 +0930

    Update WebController.php

--- app/components/WebController.php
+++ app/components/WebController.php
@@ -4,12 +4,6 @@
  * WebController
  * 
  * @mixin YdWebControllerBehavior
- *
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt 
  */
 class WebController extends Controller
 {
