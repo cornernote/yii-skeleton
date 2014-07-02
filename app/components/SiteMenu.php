66200ab1d40cd8ccd30363275480c0add94a69ec
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:29:06 +0930

    Update SiteMenu.php

--- app/components/SiteMenu.php
+++ app/components/SiteMenu.php
@@ -2,12 +2,6 @@
 
 /**
  * SiteMenu
- * 
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 class SiteMenu
 {
