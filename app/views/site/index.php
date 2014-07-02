c907dfc800a561fd5d7470ce0d69e61aebc431bf
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:32:03 +0930

    Update index.php

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -1,12 +1,6 @@
 <?php
 /**
  * @var $this SiteController
- *
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 
 $this->pageTitle = Yii::app()->name;
