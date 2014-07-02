8eba88e49443b5179d26293aeaad4edb918bd672
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:28:59 +0930

    Update Controller.php

--- app/components/Controller.php
+++ app/components/Controller.php
@@ -1,12 +1,6 @@
 <?php
 /**
  * Controller
- *
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 class Controller extends CController
 {
