228f10421209b27e1c8f26ed5cb94f535c98e699
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:31:21 +0930

    Update main.php

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -4,12 +4,6 @@
  *
  * @var $this YdWebController
  * @var $content
- *
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 ?><!DOCTYPE html>
 <html lang="<?php echo Yii::app()->language; ?>">
