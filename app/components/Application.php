51a974e511c14c862a369fd65dc23ecdeee35dc3
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:28:54 +0930

    Update Application.php

--- app/components/Application.php
+++ app/components/Application.php
@@ -22,12 +22,6 @@
  * @property TbApi $bootstrap
  * 
  * @method CClientScript getClientScript() getClientScript()
- *
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 class Application extends CWebApplication
 {
