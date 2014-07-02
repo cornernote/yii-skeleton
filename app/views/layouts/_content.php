a5d2f7361827a384656d0a673d27d2db883ea7ff
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:30:26 +0930

    Update _content.php

--- app/views/layouts/_content.php
+++ app/views/layouts/_content.php
@@ -2,12 +2,6 @@
 /**
  * @var $this WebController
  * @var $content string
- *
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 
 if ($this->showNavBar) {
