a16fffc5faa5536aab5fc48fb96b10d1cc54b2ec
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:31:55 +0930

    Update error.php

--- app/views/site/error.php
+++ app/views/site/error.php
@@ -2,12 +2,6 @@
 /**
  * @var SiteController $this
  * @var string $message
- *
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
 
 $this->pageTitle = Yii::t('app', 'Error');
