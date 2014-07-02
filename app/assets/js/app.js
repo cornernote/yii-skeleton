b1ab274c2d8bbec2a5092dadd8cb0230aa4c579d
Brett O'Donnell <cornernote@gmail.com>
2014-07-02 12:25:07 +0930

    Update app.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,9 +1,3 @@
 /**
  * Application JavaScript
- *
- * @author Brett O'Donnell <cornernote@gmail.com>
- * @author Zain Ul abidin <zainengineer@gmail.com>
- * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-skeleton
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-skeleton/master/license.txt
  */
