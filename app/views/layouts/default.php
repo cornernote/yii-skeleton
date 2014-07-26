535a54ec90818f51aa16cb279fa33d8b05a689f4
Brett O'Donnell <cornernote@gmail.com>
2014-07-26 13:56:20 +0930

    Update default.php

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -11,13 +11,13 @@
  */
 
 if (Yii::app()->request->isAjaxRequest) {
-    $this->beginContent('/layouts/ajax');
+    $this->beginContent('//layouts/ajax');
     echo user()->multiFlash();
     echo $content;
     $this->endContent();
 }
 else {
-    $this->beginContent('/layouts/main');
-    $this->renderPartial('/layouts/_content', array('content' => $content));
+    $this->beginContent('//layouts/main');
+    $this->renderPartial('//layouts/_content', array('content' => $content));
     $this->endContent();
 }
