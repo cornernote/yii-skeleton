4794fdc4460b2e95961ac8d4566dc1639e28b65c
Brett O'Donnell <cornernote@gmail.com>
2014-07-26 14:09:41 +0930

    Update SiteController.php

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -15,8 +15,12 @@ class SiteController extends YdWebController
     {
         return array(
             array('allow',
-                'actions' => array('index', 'page', 'error'),
+                'actions' => array('index', 'page', 'error', 'login', 'logout', 'lostPassword', 'resetPassword', 'signUp', 'activate', 'resendActivation', 'hybridAuth'),
                 'users' => array('*'),
+            ),
+            array('allow',
+                'actions' => array('overview', 'account', 'updateAccount', 'changePassword'),
+                'users' => array('@'),
             ),
             array('allow',
                 'actions' => array('clearCache'),
@@ -43,6 +47,40 @@ class SiteController extends YdWebController
             // They can be accessed via: index.php?r=site/page&view=FileName
             'page' => array(
                 'class' => 'CViewAction',
+            ),
+            // account actions
+            'signUp' => array(
+                'class' => 'account.actions.AccountSignUpAction',
+            ),
+            'activate' => array(
+                'class' => 'account.actions.AccountActivateAction',
+            ),
+            'resendActivation' => array(
+                'class' => 'account.actions.AccountResendActivationAction',
+            ),
+            'lostPassword' => array(
+                'class' => 'account.actions.AccountLostPasswordAction',
+            ),
+            'resetPassword' => array(
+                'class' => 'account.actions.AccountResetPasswordAction',
+            ),
+            'login' => array(
+                'class' => 'account.actions.AccountLoginAction',
+            ),
+            'logout' => array(
+                'class' => 'account.actions.AccountLogoutAction',
+            ),
+            'account' => array(
+                'class' => 'account.actions.AccountViewAction',
+            ),
+            'updateAccount' => array(
+                'class' => 'account.actions.AccountUpdateAction',
+            ),
+            'changePassword' => array(
+                'class' => 'account.actions.AccountChangePasswordAction',
+            ),
+            'hybridAuth' => array(
+                'class' => 'account.actions.AccountHybridAuthAction',
             ),
         );
     }
